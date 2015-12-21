004e3d514e722f0a74d3feb149d5f3ffa7339e8e
liuling19901127 <476777389@qq.com>
2015-12-21 17:44:10 +0800

    '添加侧滑关闭activity功能'

---
+++ app/src/main/java/com/lauren/simplenews/utils/DensityUtil.java
@@ -0,0 +1,74 @@
+package com.lauren.simplenews.utils;
+
+import java.util.concurrent.Executors;
+
+import android.content.Context;
+
+public class DensityUtil {
+
+	
+	
+	public static final int getHeightInPx(Context context) {
+		final int height = context.getResources().getDisplayMetrics().heightPixels;
+		return height;
+	}
+
+	public static final int getWidthInPx(Context context) {
+		final int width = context.getResources().getDisplayMetrics().widthPixels;
+		return width;
+	}
+
+	public static final int getHeightInDp(Context context) {
+		final float height = context.getResources().getDisplayMetrics().heightPixels;
+		int heightInDp = px2dip(context, height);
+		return heightInDp;
+	}
+
+	public static final int getWidthInDp(Context context) {
+		final float width = context.getResources().getDisplayMetrics().widthPixels;
+		int widthInDp = px2dip(context, width);
+		return widthInDp;
+	}
+
+	public static int dip2px(Context context, float dpValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dpValue * scale + 0.5f);
+	}
+
+	public static int px2dip(Context context, float pxValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (pxValue / scale + 0.5f);
+	}
+
+	public static int px2sp(Context context, float pxValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (pxValue / scale + 0.5f);
+	}
+
+	public static int sp2px(Context context, float spValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (spValue * scale + 0.5f);
+	}
+	
+	/**
+	 * 获得状态栏的高度
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static int getStatusHeight(Context context) {
+
+		int statusHeight = -1;
+		try {
+			Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+			Object object = clazz.newInstance();
+			int height = Integer.parseInt(clazz.getField("status_bar_height")
+					.get(object).toString());
+			statusHeight = context.getResources().getDimensionPixelSize(height);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return statusHeight;
+	}
+
+}
---
+++ swipeback/build/generated/source/buildConfig/release/me/imid/swipebacklayout/lib/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.imid.swipebacklayout.lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "me.imid.swipebacklayout.lib";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
