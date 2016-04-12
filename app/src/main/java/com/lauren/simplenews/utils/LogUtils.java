0b0e2d5936ac16c33a9ee28c22caf6a2651a2940
xxmbaobao <250990918@qq.com>
2016-04-12 23:58:09 +0800

    Update LogUtils.java
    add overload func for easy to add logs.

--- app/src/main/java/com/lauren/simplenews/utils/LogUtils.java
+++ app/src/main/java/com/lauren/simplenews/utils/LogUtils.java
@@ -11,40 +11,65 @@ import android.util.Log;
  */
 public class LogUtils {
     public static final boolean DEBUG = true;
-
+    private static final String TAG = "SimpleNews";
     public static void v(String tag, String message) {
         if(DEBUG) {
             Log.v(tag, message);
         }
     }
-
+    public static void v( String message) {
+        if(DEBUG) {
+            Log.v(TAG, message);
+        }
+    }
     public static void d(String tag, String message) {
         if(DEBUG) {
             Log.d(tag, message);
         }
     }
-
+      public static void d( String message) {
+        if(DEBUG) {
+            Log.d(TAG, message);
+        }
+    }
     public static void i(String tag, String message) {
         if(DEBUG) {
             Log.i(tag, message);
         }
     }
-
+     public static void i( String message) {
+        if(DEBUG) {
+            Log.i(TAG, message);
+        }
+    }
     public static void w(String tag, String message) {
         if(DEBUG) {
             Log.w(tag, message);
         }
     }
-
+    public static void w( String message) {
+        if(DEBUG) {
+            Log.w(TAG, message);
+        }
+    }
     public static void e(String tag, String message) {
         if(DEBUG) {
             Log.e(tag, message);
         }
     }
-
+      public static void e( String message) {
+        if(DEBUG) {
+            Log.e(TAG, message);
+        }
+    }
     public static void e(String tag, String message, Exception e) {
         if(DEBUG) {
             Log.e(tag, message, e);
+        }
+    }
+    public static void e(String message, Exception e) {
+        if(DEBUG) {
+            Log.e(TAG, message, e);
         }
     }
 }
